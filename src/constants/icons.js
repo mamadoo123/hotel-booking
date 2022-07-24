503fccb7d1e5a07de6d1078eacb11489f78b16db
Mohamed Khamis <mkhs66662000@gmail.com>
2022-07-25 00:35:49 +0200

    application backbone && added icon images and constants

---
+++ src/constants/icons.js
@@ -0,0 +1,17 @@
+const award = require("../assets/icons/award.png");
+const back_arrow = require("../assets/icons/left_arrow.png");
+const camera = require("../assets/icons/camera.png");
+const failed = require("../assets/icons/failed.png");
+const indicator = require("../assets/icons/indicator.png");
+const star = require("../assets/icons/star.png");
+const success = require("../assets/icons/success.png");
+
+export default {
+    award,
+    back_arrow,
+    camera,
+    failed,
+    indicator,
+    star,
+    success
+}
